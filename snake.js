21650ea603901f8a29dd25be1e649bee874ac9f1
Thijs Nijhof <tnijhof91@gmail.com>
2018-07-09 22:09:25 +0100

    added eventListener

--- snake.js
+++ snake.js
@@ -1,4 +1,11 @@
 const canvas = document.getElementById('canvas');
 const canvasContext = canvas.getContext("2d");
 canvasContext.fillStyle='#fff';
-canvasContext.fillRect(250,250,5,5);
+const snake = canvasContext.fillRect(250,250,5,5);
+
+const checkPressedKey = (event) => {
+    let key = event.keyCode;
+    console.log(key);
+}
+canvas.addEventListener('keydown', checkPressedKey);
+
